5b4d058270c2feda4ac148cd36f499aabd77b3d9
Alessio Coltellacci <lightplay8@gmail.com>
2018-11-06 01:14:33 +0100

    Add unit test to cover the steering behaviors: flee, seek, pursuit, evade

---
+++ src/test/scala/com/agar/core/behaviors/BehaviorsSpec.scala
@@ -0,0 +1,41 @@
+package com.agar.core.behaviors
+
+import com.agar.core.behaviors.Behavior.TargetEntity
+import com.agar.core.utils.Vector2d
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.language.postfixOps
+
+
+class BehaviorsSpec() extends FlatSpec with Matchers {
+
+  val MAX_VELOCITY = 3:Short
+
+  "An entity" should "move toward the target with steer behavior" in {
+    val target = new Vector2d(40, 70)
+    val pos = new Vector2d(80, 60)
+    val vel = new Vector2d(2, 2)
+    Behavior.seek(target, pos, vel, MAX_VELOCITY) should be (new Vector2d(-4.910427500435995, -1.2723931248910012))
+  }
+
+  "An entity" should "run away from the target with flee behavior" in {
+    val target = new Vector2d(90, 60)
+    val pos = new Vector2d(80, 60)
+    val vel = new Vector2d(2, 2)
+    Behavior.flee(target, pos, vel, MAX_VELOCITY) should be (new Vector2d(-5, -2))
+  }
+
+  "An entity" should "be able to pursuit a target" in {
+    val target = new TargetEntity(new Vector2d(40, 70), new Vector2d(2, 2))
+    val pos = new Vector2d(80, 60)
+    val vel = new Vector2d(2, 2)
+    Behavior.pursuit(target, pos, vel, MAX_VELOCITY) should be (new Vector2d(-4.567868516922847, -3.5511451511049703))
+  }
+
+  "An entity" should "be evade from a pursuer" in {
+    val target = new TargetEntity(new Vector2d(40, 70), new Vector2d(2, 2))
+    val pos = new Vector2d(80, 60)
+    val vel = new Vector2d(2, 2)
+    Behavior.evade(target, pos, vel, MAX_VELOCITY) should be (new Vector2d(0.5678685169228461, -0.44885484889502947))
+  }
+}
