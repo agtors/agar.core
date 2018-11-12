3689d838b3a0018fd3262ba60541533b547e5158
Alessio Coltellacci <lightplay8@gmail.com>
2018-11-12 22:18:39 +0100

    Rename logger for journal in Agar

--- src/main/scala/com/agar/core/Agar.scala
+++ src/main/scala/com/agar/core/Agar.scala
@@ -5,7 +5,7 @@ import akka.actor.{ActorRef, ActorSystem}
 import com.agar.core.context.{AgarSystem, DefaultAgarSystem}
 import com.agar.core.arbritrator.Arbitrator
 import com.agar.core.arbritrator.Player.StartGameTurn
-import com.agar.core.logger.Logger
+import com.agar.core.logger.Journal
 import com.agar.core.region.Region
 import com.agar.core.region.Region.InitRegion
 
@@ -16,7 +16,7 @@ object Agar extends App {
   implicit val context: AgarSystem = DefaultAgarSystem
 
   implicit val system: ActorSystem = ActorSystem("Agar")
-  val journal: ActorRef = system.actorOf(Logger.props)
+  val journal: ActorRef = system.actorOf(Journal.props)
 
   // A region has a size of 4 screen 1920x1080
   val region = system.actorOf(Region.props(journal, 7680, 4320), "region")
